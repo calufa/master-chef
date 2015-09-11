037cea7e5c1d18d789d9c4363b6e0b94b1ea068f
Carlos <calufa@gmail.com>
2015-09-11 14:19:52 -0500

    changes

--- web/app/controllers/Application.java
+++ web/app/controllers/Application.java
@@ -1,14 +1,54 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
 
-import views.html.*;
+
+
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import play.mvc.Controller;
+import play.mvc.Result;
+import rabbit.utils.Download;
+import views.html.index;
+
+
+
 
 public class Application extends Controller {
-  
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-  
+
+
+
+
+	public static Result index() {
+		return ok(index.render());
+	}
+
+
+
+
+	public static Result search(String query){
+
+		try{
+			
+			response().setHeader("Access-Control-Allow-Origin", "*"); 
+			response().setHeader("Content-Type", "application/json");
+
+			String url = "http://104.197.48.101:8983/solr/master-chef/select?q=" + query + "&wt=json&rows=50";
+			
+			System.out.println(url);
+			
+			JSONObject json = new Download().getJSON(url);
+			
+			JSONArray results = json.getJSONObject("response").getJSONArray("docs");
+			
+			return ok(results.toString());
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
 }
